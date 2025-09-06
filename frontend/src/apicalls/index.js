f5e945f0116ff71e506ebfc632d5b77b7761863f
Lakshman kumare <lakshmans356@gmail.com>
2025-09-06 15:57:50 +0530

    fixed url

--- frontend/src/apicalls/index.js
+++ frontend/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:5000',
+    baseURL: 'http://https://entrance-exam-portal.onrender.com',
     headers: {
        'authorization': `Bearer ${localStorage.getItem('token')}`
     }
